21f7b1f1dfd9754ab3da0262bf83ac4958022dc9
trkk28097402 <96816920+trkk28097402@users.noreply.github.com>
2023-05-09 01:38:05 +0800

    Create job_script.sh

---
+++ supercell_11layer/job_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=iscqe
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=72
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20g
+#SBATCH --time=01:00:00
+#SBATCH --output=%j.out
+
+source $HOME/.bashrc
+
+module load intel/2021.7.0
+module load mkl/2022.2.1 m4/1.4.19
+module load git/2.38.1
+module load intelmpi/2021.7.1
+
+export BIN_PATH=$HOME/QE
+export TESTCASE_PATH=$HOME/QE/ISC23-qe
+
+cd $HOME/QE/ISC23-qe/supercell_11layer
+
+mpirun $BIN_PATH/bin/cp.x -inp cp.in -nb 2
